44da332fe3a4a6bf6fb0f9389aeeb623b2522628
Mpho Matli <mphojames9@gmail.com>
2023-08-13 16:30:05 +0200

    update

--- script.js
+++ script.js
@@ -1,122 +1,69 @@
-const tiles = document.querySelectorAll('.tile');
-const PLAYER_ONE = 'X';
-const PLAYER_TWO = 'O';
-let turn = PLAYER_ONE;
+const tiles = document.querySelectorAll(".tile");
+const player_X = "X";
+const player_O = "O";
+const gameOverSec = document.querySelector(".gameOver");
+const game = document.querySelector(".game");
+const results = document.querySelector(".WinnerText");
+let turn = player_X;
 
 const boardState = Array(tiles.length);
 boardState.fill(null);
 
-//elements
-const strike = document.getElementById('strike');
-const gameOverArea = document.getElementById('game-over-area');
-const gameOverText = document.getElementById('game-over-text');
-const playAgain = document.getElementById('play-again');
-const clickSound = new Audio ("sounds/click.wav");
-const gameOverSound = new Audio ("sounds/gameover.wav");
-
-//Adding eventListener to each tile
-tiles.forEach((tile)=>tile.addEventListener('click', tileClick));
-
-function setHoverText(){
-    //remove any existing hover text
-    tiles.forEach((tile) => {
-        tile.classList.remove("x-hover");
-        tile.classList.remove("o-hover");
-    });
-
-    const hoverClass = `${turn.toLowerCase()}-hover`;
-    tiles.forEach((tile) => {
-        if (tile.innerText == "") {
-            tile.classList.add(hoverClass);
-        }
-    })
+function startGame(){
+    tiles.forEach(tile => tile.addEventListener('click', tileClick))    
 }
 
-setHoverText()
-function tileClick(e){
-    if (gameOverArea.classList.contains('visible')){
+function tileClick(e) {
+    if(gameOverSec.classList.contains("visible")){
         return;
-    }
-
-    const tile = e.target;
-    const tileNumber = tile.dataset.index
-    if(tile.innerText !=""){
-        return;
-    }
-// Player one's turn
-    if (turn === PLAYER_ONE){
-        tile.innerText = PLAYER_ONE;
-        boardState [tileNumber-1] = PLAYER_ONE;
-        turn = PLAYER_TWO;
-    }
-// player two's turn
-    else{
-        tile.innerText = PLAYER_TWO;
-        boardState [tileNumber-1] = PLAYER_TWO;
-        turn = PLAYER_ONE
-    }
-    clickSound.play();
-    setHoverText();
-    checkWinner();
-}
-
-function checkWinner() {
-    for (const winningCombination of winningCombinations) {
-        const { combo, strikeClass } = winningCombination;
-        const tileValue1 = boardState[combo[0] - 1];
-        const tileValue2 = boardState[combo[1] - 1];
-        const tileValue3 = boardState[combo[2] - 1];
-
-
-    if(tileValue1 != null &&
-        tileValue1 === tileValue2 &&
-        tileValue1 === tileValue3) {
-            strike.classList.add(strikeClass);
-            gameOverScreen (tileValue1);
-            return;
+    }else{
+    const id = e.target.id
+    if(!boardState[id-1]){
+        boardState[id-1] = turn;
+        e.target.innerText = turn;
+        if(checkWinner() !==false){
+                results.innerHTML = `Player ${turn} wins`;
+                gameOverSec.classList.add("visible");
+                game.style.opacity = "0.07";
+                return;
         }
 
-// I am going to define a draw function
-const allTileFilledIn = boardState.every((tile) =>tile !== null);
-if (allTileFilledIn){
-    gameOverArea.className = "visible";
-    gameOverText.innerText = 'Draw Game'};
-}};
+        turn = turn == player_X ? player_O : player_X;
+        const allTilesFilledIn = boardState.every((tile) => tile !== null);
+        if(allTilesFilledIn){
+            results.innerHTML = "It's a Tie";
+            gameOverSec.classList.add("visible");
+            game.style.opacity = "0.07";
+    }}
+}}
 
-
-//Winning function
-function gameOverScreen(winnerText){
-    if (winnerText != null);
-    {
-        text = `Winner is ${winnerText}!`;
-    }
-    gameOverSound.play();
-    gameOverArea.className = "visible";
-    gameOverText.innerText = text;
-}
-
-
-
-//setting winning combinations
 
 const winningCombinations = [
-    //Rows
-    { combo: [1,2,3], strikeClass: "strike-row-1"},
-    { combo: [4,5,6], strikeClass: "strike-row-2"},
-    { combo: [7,8,9], strikeClass: "strike-row-3"},
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
 
-    //Colunms
-    { combo: [1,4,7], strikeClass:"strike-column-1"},
-    { combo: [2,5,8], strikeClass:"strike-column-2"},
-    { combo: [3,6,9], strikeClass:"strike-column-3"},
-
-    //diagonals
-    {combo: [1,5,9], strikeClass: "strike-diagonal-1"},
-    {combo: [3,5,7], strikeClass: "strike-diagonal-2"},
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    
+    [0,4,8],
+    [2,4,6],
 ]
 
-//Play again or restart button
-playAgain.addEventListener('click', function() {
-    location.reload();
-});
+function checkWinner(){
+        for (const condition of winningCombinations){
+            let [a,b,c] = condition;
+            if(boardState[a] && (boardState[a] == boardState[b] && boardState[a] == boardState[c])){
+                return[a,b,c]
+            }
+        }
+        return false;
+    };
 
+
+function reset(){
+    location.reload();
+}
+
+startGame();
